7f46dfccede2b841bb14d8f30a5e0d49883edbe8
GaiaFaberi <faberi.gaia@itismeucci.com>
2024-04-11 00:10:52 +0200

    fine

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//mappa
 const map = L.map('map').setView([41.89308809955699, 12.487235760915924], 7);
 let btn = document.getElementById("trovaN");
 
@@ -22,4 +23,31 @@ function creaMarker(){
     }
 }
 
+//grafico
+
+const ctx = document.getElementById('myChart');
+
+new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['Xiaomi Note 11S', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+})
+
+
+//funzioni mappa
 btn.onclick = creaMarker;
+
+//funzioni grafico
